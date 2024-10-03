35ea29c082dfdcb9151a383031ed0094a9e0f374
Endeavour <martin.wonders@unn.ac.uk>
2024-10-03 20:13:28 +0100

    Added comments

--- KV5047_AdventureGame/KV5047_AdventureGame.cpp
+++ KV5047_AdventureGame/KV5047_AdventureGame.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-	
+	//A 10 by 10 2 dimensial array using list initialisation
+	//'=' = water, 'g' = grass, 'H' = walls, 'T' = trees
 	char adventureMap[10][10] = {
 		{'=', '=', '=', '=', '=', '=', '=', '=', '=', '='},
 		{'=', '=', 'g', 'g', 'g', 'g', 'g', '=', '=', '='},
@@ -16,13 +17,14 @@ int main()
 		{'=', '=', '=', 'g', 'T', 'T', '=', '=', '=', '='}
 	};
 
-
-
-
+	//Loop through the row indices from 0 to 10
 	for (int row = 0; row < 10; row++)
 	{
+		//Loop through the column indices from 0 to 10 while 
+		//the current row index remains fixed
 		for (int column = 0; column < 10; column++)
 		{
+			//use row and column to index into the 2D array
 			std::cout << adventureMap[row][column] << " ";
 		}
 		std::cout << std::endl;
